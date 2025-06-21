edce98a8862ffaa60f43da464719f9fd988675db
zsomii <ed1lord1@gmail.com>
2025-06-21 02:44:09 +0200

    g

--- frontend/src/Quiz.js
+++
@@ -1,8 +0,0 @@
-{showResult && (
-  <div style={{ textAlign: 'center' }}>
-    <h2>Gratulálunk, {name}!</h2>
-    <p>{score} pontot szereztél ebben az órában.</p>
-  </div>
-)}
-
-export default Quiz;
